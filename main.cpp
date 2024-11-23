f2b24e233e6610e1134eb8c008e4111a474dca03
Andrew <115037162+StowPO@users.noreply.github.com>
2024-11-22 21:22:09 -0500

    small changes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "graphics.h"
 #include "logic.h"
+#include "graphics.h"
 #include "ui.h"
 
 #include "FEHLCD.h"
@@ -197,6 +197,10 @@ int credits()
     }
 }
 
+/*
+ * logic.h implementations
+ */
+
 /* Game Methods */
 
 void Game::initialize()
@@ -216,21 +220,12 @@ void Game::cleanup()
     // TODO
 }
 
-
-
-/*
- * logic.h implementations
- */
-
 Level Game::currentLevel;
 Position Player::position;
 
 void Player::render(struct Position screenPosition) {}
 void Tile::render(struct Position screenPosition) const {}
 void Collectible::render(struct Position screenPosition) const {}
-
-
-
 
 void Logic::updateLogic()
 {
@@ -294,104 +289,6 @@ Level::~Level()
 {
     free(&this->tiles);
     free(&this->collectibles);
-}
-
-/*
- * render.h implementations
- */
-
-Position Camera::origin = {0, 0};
-
-Position Camera::getScreenPosition(const Position &gamePosition)
-{
-    // Initialize return struct on the stack.
-    Position screenPosition;
-
-    // Offset the game position by the camera's origin.
-    screenPosition.x = gamePosition.x - Camera::origin.x;
-    screenPosition.y = gamePosition.y - Camera::origin.y;
-
-    // Return a copy of the resulting screen position.
-    return screenPosition;
-}
-
-bool Camera::isInFrame(const Position &screenPosition, const int spriteWidth, const int spriteHeight)
-{
-    // Check if the game object is within the camera's bounds.
-    if (screenPosition.x + spriteWidth < 0 ||  // Is the sprite to the left of the camera?
-        screenPosition.x > PROTEUS_WIDTH ||    // Is the sprite to the right of the camera?
-        screenPosition.y + spriteHeight < 0 || // Is the sprite above the camera?
-        screenPosition.y > PROTEUS_HEIGHT)     // Is the sprite below the camera?
-    {
-        // The game object is outside one of the camera's bounds.
-        return false;
-    }
-    else
-    {
-        // The game object is within all of the camera's bounds.
-        return true;
-    }
-}
-bool Camera::isInFrame(const Position &screenPosition)
-{
-    // Return result of overloaded method with default values.
-    return Camera::isInFrame(screenPosition, DEFAULT_SPRITE_SIZE, DEFAULT_SPRITE_SIZE);
-}
-
-void Camera::follow(const Position &targetPosition, const int spriteWidth, const int spriteHeight)
-{
-    // Set the camera's origin one half-screen above and to the left of the sprite's origin.
-    Camera::origin.x = targetPosition.x - PROTEUS_WIDTH / 2;
-    Camera::origin.y = targetPosition.y - PROTEUS_HEIGHT / 2;
-
-    // Offset the camera's position by the sprite's size,
-    // so the center of the camera is at the center of the sprite.
-    Camera::origin.x += spriteWidth / 2;
-    Camera::origin.y += spriteHeight / 2;
-}
-void Camera::follow(const Position &targetPosition)
-{
-    // Return result of overloaded method with default values.
-    Camera::follow(targetPosition, DEFAULT_SPRITE_SIZE, DEFAULT_SPRITE_SIZE);
-}
-
-void Graphics::render()
-{
-    // Ensure the camera stays centered on the player
-    // during this rendering cycle.
-    Camera::follow(Player::position);
-
-    // Iterate through every tile in the level.
-    for (const Tile tile : Game::currentLevel.tiles)
-    {
-        // Find the screen position of the current tile.
-        Position screenPosition = Camera::getScreenPosition(tile.position);
-
-        // Render the tile if the camera can see it.
-        if (Camera::isInFrame(screenPosition)) {
-            tile.render(screenPosition);
-        }
-    }
-
-    // Iterate through every collectible in the level.
-    for (const Collectible collectible : Game::currentLevel.collectibles)
-    {
-        // Find the screen position of the current collectible.
-        Position screenPosition = Camera::getScreenPosition(collectible.position);
-
-        // Render the collectible if the camera can see it.
-        if (Camera::isInFrame(screenPosition)) {
-            collectible.render(screenPosition);
-        }
-    }
-
-    // Find the screen position of the player.
-    Position screenPosition = Camera::getScreenPosition(Player::position);
-
-    // Render the player to the screen.
-    // No need to check if the player is in frame
-    // because they are always in frame.
-    Player::render(screenPosition);
 }
 
 /*
