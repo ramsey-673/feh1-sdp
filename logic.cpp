9e9520af0f5234c10059b263924e2b58e24168f9
Nathan Ramsey <ramsey.673@buckeyemail.osu.edu>
2024-11-27 23:56:26 -0500

    New stone + water textures

--- logic.cpp
+++ logic.cpp
@@ -417,6 +417,7 @@ void Game::initialize() {
     Level::tileFileMap.insert({'d', "ttextures/dirt.png"});
     Level::tileFileMap.insert({'g', "ttextures/grass.png"});
     Level::tileFileMap.insert({'s', "ttextures/stone.png"});
+    Level::tileFileMap.insert({'S', "ttextures/stone_top.png"});
     Level::tileFileMap.insert({'b', "ttextures/stone_bricks.png"});
     Level::tileFileMap.insert({'c', "ctextures/dollar.png"});
     Level::tileFileMap.insert({'n', "ntextures/scooter.png"});
